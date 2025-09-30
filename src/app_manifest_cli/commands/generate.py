cc4eb50dd86fc7671706e7ca8ab6dfd8fa06c9e0
borislavr <boris.lavr@gmail.com>
2025-09-30 09:44:06 +0300

    feat: added output

--- src/app_manifest_cli/commands/generate.py
+++ src/app_manifest_cli/commands/generate.py
@@ -1,4 +1,4 @@
-import json, yaml, typer
+import json, yaml, typer, os
 from ..commands.create import create_command
 from ..commands.create import get_bom_ref
 from ..services.components import add_component as _add_component
@@ -16,76 +16,82 @@ def generate_command(
     name: str = typer.Option(None, "--name", "-n", help="Application name"),
     version: str = typer.Option(None, "--version", "-v", help="Application version"),
     out: Optional[str] = typer.Option(None, "--out", "-o", help="Output file (default: stdout)"),
-    # TODO: Убрать этот параметр:
-    # discovery: bool = typer.Option(False, "--discovery", help="Enable component discovery"),
 ) -> None:
-    configuration_data = load_configuration(configuration)
-    if version == None and "applicationVersion" in configuration_data:
-        version = configuration_data["applicationVersion"]
-    if name == None and "applicationName" in configuration_data:
-        name = configuration_data["applicationName"]
-    if out == None:
-        out = name + '-' + version + '.json'
-    body = create_command(name=name, version=version, out=open(out, "w"))
-    # Получаю компоненты из конфига -- именно они определяют состав манифеста
-    config_components = get_components_from_data(configuration_data)
-    # Получаю депенденси из конфига
-    config_dependencies = load_dependencies(config_components)
-    # Читаю все json файлы с компонентами
-    json_components = []
-    if components_files:
-        for json_path in components_files:
-            with open(json_path.resolve(), "r", encoding="utf-8") as f:
-                json_comp = json.load(f)
-                json_components.append(json_comp)
-    # Перебираю компоненты из конфига, если в json_components есть такой же, то обновляю его
-    components = []
-    for conf_comp in config_components:
-        conf_comp['mime-type'] = conf_comp["mimeType"]
-        for json_comp in json_components:
-            if conf_comp["name"] == json_comp["name"] and conf_comp["mime-type"] == json_comp["mime-type"]:
-                typer.echo(f"Found update for component {conf_comp['name']}")
-                conf_comp.update(json_comp)
-                typer.echo(f"Updated component: {conf_comp}")
-        if "bom-ref" not in conf_comp:
-            conf_comp["bom-ref"] = get_bom_ref(conf_comp["name"])
-        components.append(conf_comp)
+    try:
+        if configuration is None:
+            raise ValueError("Configuration file must be provided with --config option")
+        configuration_data = load_configuration(configuration)
+        if version == None and "applicationVersion" in configuration_data:
+            version = configuration_data["applicationVersion"]
+        if name == None and "applicationName" in configuration_data:
+            name = configuration_data["applicationName"]
+        if out == None:
+            out = name + '-' + version + '.json'
+        body = create_command(name=name, version=version, out=open(out, "w"))
+        # Получаю компоненты из конфига -- именно они определяют состав манифеста
+        config_components = get_components_from_data(configuration_data)
+        # Получаю депенденси из конфига
+        config_dependencies = load_dependencies(config_components)
+        # Читаю все json файлы с компонентами
+        json_components = []
+        if components_files:
+            for json_path in components_files:
+                with open(json_path.resolve(), "r", encoding="utf-8") as f:
+                    json_comp = json.load(f)
+                    json_components.append(json_comp)
+        # Перебираю компоненты из конфига, если в json_components есть такой же, то обновляю его
+        components = []
+        for conf_comp in config_components:
+            conf_comp['mime-type'] = conf_comp["mimeType"]
+            for json_comp in json_components:
+                if conf_comp["name"] == json_comp["name"] and conf_comp["mime-type"] == json_comp["mime-type"]:
+                    typer.echo(f"Found update for component {conf_comp['name']}")
+                    conf_comp.update(json_comp)
+                    typer.echo(f"Updated component: {conf_comp}")
+            if "bom-ref" not in conf_comp:
+                conf_comp["bom-ref"] = get_bom_ref(conf_comp["name"])
+            components.append(conf_comp)
 
-    # Генерирую дополнительные метаданные для helm chart
-    #   добавляю properies с именем qubership:helm.values.artifactMappings
-    #   и значением в виде dict, где ключ - bom-ref зависимости, а значение - dict с ключом valuesPathPrefix и значением из конфигурации
+        # Генерирую дополнительные метаданные для helm chart
+        #   добавляю properies с именем qubership:helm.values.artifactMappings
+        #   и значением в виде dict, где ключ - bom-ref зависимости, а значение - dict с ключом valuesPathPrefix и значением из конфигурации
 
-    components = generate_helm_values_artifact_mappings(components)
-    # Если включено discovery, то запускаю его
-    # дополняя компоненты зависимостями и свойствами
-    # if discovery:
-    components = helm_discovery(components)
-    # Добавляю все компоненты в манифест
-    for comp in components:
-        if comp.get("mime-type") == "application/vnd.qubership.standalone-runnable":
-            if comp.get("version", "") == "":
-                comp["version"] = version
-        typer.echo(f"Adding component: {comp['name']} with mime-type: {comp['mime-type']}")
-        #typer.echo(f"comp details: {json.dumps(comp, indent=2)}")
-        _add_component(manifest_path=out, payload_text=json.dumps(obj=comp,sort_keys=True), out_file=None)
-    # Формирую простой список компонент для удобства поиска
-    components_list = [ comp["mime-type"] + ":" + comp["name"] for comp in components]
-    for dep in config_dependencies:
-        dep_record = {}
-        if "ref" not in dep:
-            raise ValueError("Each dependency must have a 'ref' field")
-        if "dependsOn" not in dep:
-            raise ValueError("Each dependency must have a 'dependsOn' field")
-        if dep["ref"] not in components_list:
-            raise ValueError(f"Dependency ref '{dep['ref']}' not found in components")
-        dep_record["ref"] = next(comp for comp in components if (comp["mime-type"] + ":" + comp["name"]) == dep["ref"])["bom-ref"]
-        dep_record["dependsOn"] = []
-        for d in dep["dependsOn"]:
-            if d not in components_list:
-                raise ValueError(f"Dependency dependsOn '{d}' not found in components")
-            depends_on_component = next(comp for comp in components if (comp["mime-type"] + ":" + comp["name"]) == d)
-            dep_record["dependsOn"].append(depends_on_component["bom-ref"])
-        _add_dependency(manifest_path=out, payload_text=json.dumps(dep_record), configuration=None, out_file=None)
+        components = generate_helm_values_artifact_mappings(components)
+        # Если включено discovery, то запускаю его
+        # дополняя компоненты зависимостями и свойствами
+        # if discovery:
+        components = helm_discovery(components)
+        # Добавляю все компоненты в манифест
+        for comp in components:
+            if comp.get("mime-type") == "application/vnd.qubership.standalone-runnable":
+                if comp.get("version", "") == "":
+                    comp["version"] = version
+            typer.echo(f"Adding component: {comp['name']} with mime-type: {comp['mime-type']}")
+            #typer.echo(f"comp details: {json.dumps(comp, indent=2)}")
+            _add_component(manifest_path=out, payload_text=json.dumps(obj=comp,sort_keys=True), out_file=None)
+        # Формирую простой список компонент для удобства поиска
+        components_list = [ comp["mime-type"] + ":" + comp["name"] for comp in components]
+        for dep in config_dependencies:
+            dep_record = {}
+            if "ref" not in dep:
+                raise ValueError("Each dependency must have a 'ref' field")
+            if "dependsOn" not in dep:
+                raise ValueError("Each dependency must have a 'dependsOn' field")
+            if dep["ref"] not in components_list:
+                raise ValueError(f"Dependency ref '{dep['ref']}' not found in components")
+            dep_record["ref"] = next(comp for comp in components if (comp["mime-type"] + ":" + comp["name"]) == dep["ref"])["bom-ref"]
+            dep_record["dependsOn"] = []
+            for d in dep["dependsOn"]:
+                if d not in components_list:
+                    raise ValueError(f"Dependency dependsOn '{d}' not found in components")
+                depends_on_component = next(comp for comp in components if (comp["mime-type"] + ":" + comp["name"]) == d)
+                dep_record["dependsOn"].append(depends_on_component["bom-ref"])
+            _add_dependency(manifest_path=out, payload_text=json.dumps(dep_record), configuration=None, out_file=None)
+        typer.echo(f"Manifest generated and saved to {out}")
+        os.environ["PATH_APP_MANIFEST"] = out
+    except Exception as e:
+        typer.echo(f"Error: {e}")
+        raise typer.Exit(code=1)
 
 def load_configuration(configuration: str) -> dict:
     # bomFormat: CycloneDX
